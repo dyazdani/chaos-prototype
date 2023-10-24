9bfa3c9ba02769cb7c567aa72cb829b8eff8481e
Dara Yazdani <dara.s.yazdani@gmail.com>
2023-10-24 10:36:01 -0700

    Added game logic for selecting color at beginning of game

--- src/logic.ts
+++ src/logic.ts
@@ -1,30 +1,65 @@
 import type { RuneClient } from "rune-games-sdk/multiplayer"
 
+export interface PlayersObject {
+    [Player: string]: { 
+      color: string
+  }
+}
+
 export interface GameState {
-  count: number
+  allPlayerIds: string[]
+  allPlayers: PlayersObject
+  turnOrder: string[]
+  playersReady: object[]
+  colorsChosen: string[]
+  hasGameStarted: boolean
+
 }
 
 type GameActions = {
-  increment: (params: { amount: number }) => void
+  nextTurn: () => void
+  assignColor: (chosenColor: string) => void
 }
 
 declare global {
   const Rune: RuneClient<GameState, GameActions>
 }
 
-export function getCount(game: GameState) {
-  return game.count
-}
-
 Rune.initLogic({
-  minPlayers: 1,
+  minPlayers: 2,
   maxPlayers: 4,
-  setup: (): GameState => {
-    return { count: 0 }
-  },
+  setup: (allPlayerIds: string[]): GameState => {
+    return ({
+    allPlayerIds,
+    // Determine random turn order with mapped array
+    turnOrder: allPlayerIds
+      .map((value: string) => ({ value, sort: Math.random() }))
+      .sort((a, b) => a.sort - b.sort)
+      .map(({ value }) => value),
+    // An object with player objects
+    allPlayers: allPlayerIds
+      .reduce((acc, curr) => ({
+      ...acc, [curr]: {
+        color: ""}
+    }), {}),
+    playersReady: [],
+    hasGameStarted: false,
+    colorsChosen: [],
+  })},
   actions: {
-    increment: ({ amount }, { game }) => {
-      game.count += amount
+    assignColor: (chosenColor, {game, playerId}) => {
+      if (game.allPlayers[playerId].color) {
+        throw Rune.invalidAction();
+      }
+      game.allPlayers[playerId].color = chosenColor;
+      game.colorsChosen.push(chosenColor);
+      game.playersReady.push({[chosenColor]: playerId})
+    },
+    
+    nextTurn: (_, {game} ) => {
+      // Use turnOrder array to establish next player's turn
+      const slicedTurnOrder = game.turnOrder.slice(1);
+      game.turnOrder = [...slicedTurnOrder, game.turnOrder[0]];
     },
   },
 })
